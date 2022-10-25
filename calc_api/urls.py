394b2eb4de1b29b08d120501b69c003828777f18
emelyn <emelyn.freire.116@ufrn.edu.br>
2022-10-25 14:49:05 -0300

    primeira versão calculadora

---
+++ calc_api/urls.py
@@ -0,0 +1,32 @@
+"""calc_api URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from core import views #importação do app core para configurar rotas
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('hello/', views.hello_world),#retorna a função hello_world da views de core
+    #path('hello/<nome>/', views.primeiro_nome), #retorna a função primeiro_nomeda views de core
+    #path('hello/<nome>/<int:idade>/', views.nome_com_idade), #retorna a função nome_com_idadec da views de core
+
+    path('soma/<int:x>/<int:y>/', views.soma),#retorna a função hello_world da views de core
+    path('subtracao/<int:x>/<int:y>/', views.subtracao),#retorna a função hello_world da views de core
+    path('divisao/<int:x>/<int:y>/', views.divisao),#retorna a função hello_world da views de core
+    path('multiplicacao/<int:x>/<int:y>/', views.multiplicacao),#retorna a função hello_world da views de core
+
+
+]
---
+++ core/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse
+
+
+#Criação de função e visualizações dos client
+
+def soma(request, x, y):
+    return HttpResponse('Soma: {} '.format(x+y))
+
+def subtracao(request, x, y):
+    return HttpResponse('Subtração: {} '.format(x-y))
+
+def multiplicacao(request, x, y):
+    return HttpResponse('Multiplicacao: {} '.format(x*y))
+
+def divisao(request, x, y):
+    return HttpResponse('Divisão: {} '.format(x/y))
+
+
+
+
